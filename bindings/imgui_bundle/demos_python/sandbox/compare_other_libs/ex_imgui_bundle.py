474becf4e805235643226251cdb454eee36cd3b3
Pascal Thomet <pthomet@gmail.com>
2026-04-26 14:13:19 +0200

    doc: add compare page

--- bindings/imgui_bundle/demos_python/sandbox/compare_other_libs/ex_imgui_bundle.py
+++ bindings/imgui_bundle/demos_python/sandbox/compare_other_libs/ex_imgui_bundle.py
@@ -6,7 +6,7 @@ items = ["Apple", "Banana", "Cherry"]
 def gui():
     global selected_idx
     imgui.text("Choose a fruit:")
-    _changed, selected_idx = imgui.list_box("Choose a fruit", selected_idx, items)
+    _changed, selected_idx = imgui.list_box("##fruits", selected_idx, items)
     imgui.text(f"You selected: {items[selected_idx]}")
 
-hello_imgui.run(gui, window_title="Fruit Picker")
+hello_imgui.run(gui, window_title="Fruit Picker", window_size_auto=True)
